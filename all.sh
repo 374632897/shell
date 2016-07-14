ad295015fc7a0596aacd80dd81d8b4c60c97d6f6
374632897 <374632897@qq.com>
2016-07-14 14:37:09 +0800

    :smirk:

--- all.sh
+++ all.sh
@@ -1,3 +1,7 @@
-git add .
-git commit -m '$1'
-git push
+default_commit_message=':smirk:';
+if [ $1 ]
+  then default_commit_message=$1;
+fi;
+git add .;
+git commit -m $default_commit_message;
+git push;
