3c30242e76d5de0650e77b29580abebb84d9c5ed
374632897 <374632897@qq.com>
2016-07-14 14:37:53 +0800

    test

--- tests/t.sh
+++ tests/t.sh
@@ -1,6 +1,6 @@
 # @Author: Jiang Guoxi
 # @Date:   2016-07-14 12:43:05
 # @Last Modified by:   Jiang Guoxi
-# @Last Modified time: 2016-07-14 12:43:30
-
+# @Last Modified time: 2016-07-14 14:37:48
+# test;
 echo $*;
