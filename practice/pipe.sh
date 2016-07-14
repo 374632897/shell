8eaadaec90d99a4ab2d49da749d98ecedd71b80b
374632897 <374632897@qq.com>
2016-07-14 18:43:17 +0800

    :smirk:

---
+++ practice/pipe.sh
@@ -0,0 +1,12 @@
+# @Author: Jiang Guoxi
+# @Date:   2016-07-14 18:37:46
+# @Last Modified by:   Jiang Guoxi
+# @Last Modified time: 2016-07-14 18:40:41
+# cmd > file 输出至file
+# cmd < file 从file读取输入
+# cmd >> file 追加输出到file
+# n > file 文件描述符为n的文件输出至fi
+# n >> file 文件描述符为n的文件追加至fi
+# n >& m 将输出文件m和n合并
+# n <& m 将输入文件m和n合并
+# << tag 将开始标记和结束标记tag之间的内容作为输入
