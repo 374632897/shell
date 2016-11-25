dfff2097fff25f8c687b86ebf134c277046a805e
374632897 <374632897@qq.com>
2016-11-25 23:10:58 +0800

    :smirk:

--- sed/makedata.sh
+++ sed/makedata.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 cat /dev/null > data;
+cat /dev/null > data7;
 for file in {1..10}; do
   echo "The quick brown fox jumps over the lazy dog." >> data;
 done;
+for i in {1..4}; do
+  echo "This is line number $i" >> data7;
+done;
+echo "This is line number 1 again" >> data7;
+echo "Thi is stext you want to keep." >> data7;
+echo "This is the last line in t he file" >> data7; 
